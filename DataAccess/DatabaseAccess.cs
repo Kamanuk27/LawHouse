5635a7aedf27f702ff5c0f09c2bfc811116bea26
Stanislava Yudytska <syudytska27@gmail.com>
2018-11-16 11:42:29 +0100

    Alle dataAccess ting

---
+++ DataAccess/DatabaseAccess.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DataAccess
+{
+    class DatabaseAccess : IDataAccess
+    {
+        private SqlConnection connection;
+        private SqlCommand command = new SqlCommand();
+
+        // Metoden indeholder connectionstring.
+        private string GetConnectionString()
+        {
+           return ConfigurationManager.ConnectionStrings["Sql"].ToString();
+        }
+
+        //Metoden åbner SQL-connection og indeholder command.
+        private void PrepareSql(string sqlString)
+        {
+            connection = new SqlConnection(GetConnectionString());
+            connection.Open();
+            command.Connection = connection;
+            command.CommandText = sqlString;
+        }
+
+        //Metoden bruges for ExecuteNonQuery() commands, som Create, Update, Delete.
+
+        private void ExecuteSql(string sqlString)
+        {
+            PrepareSql(sqlString);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+
+        public Case GetCase(int id)
+        {
+            string sqlString = "SELECT*FORM case ... WHERE   ";
+            PrepareSql(sqlString);
+            Case c1 = new Case();
+            SqlDataReader reader = null;
+            reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                      c1.ID = reader["ti_id"] != DBNull.Value ? Convert.ToInt32(reader["ti_id"]) : default(int);
+                    
+                    }
+                }
+                connection.Close();
+                return c1;
+            }
+        }
+
+        public List<Employee> ReadAllEmployee()
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public Employee ReadSingleEmployee(string ssn)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccess/DatabaseFactory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace DataAccess
+{
+    public class DatabaseFactory
+    {
+        private static DatabaseFactory instance = null;
+        private DatabaseFactory()
+        {
+
+        }
+
+        public static DatabaseFactory Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DatabaseFactory();
+                }
+
+                return instance;
+            }
+        }
+        public Catalogue GetDatabase()
+        {
+            Catalogue empcat = null;
+            string database = ConfigurationManager.AppSettings["database"];
+            if (database == "test")
+            {
+                empcat = new Catalogue(new TestDB());
+            }
+            else if (database == "sqlserver")
+            {
+                empcat = new Catalogue(new DatabaseAccess());
+            }
+            return empcat;
+        }
+    }
+}
+}
