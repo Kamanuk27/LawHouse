2850a2fdfd4f2563caff9bb2832b68d4e4959c8f
Stanislava Yudytska <syudytska27@gmail.com>
2018-12-03 23:00:35 +0100

    HrController er public

--- BusinessLogic/HrHandler.cs
+++ BusinessLogic/HrHandler.cs
@@ -22,22 +22,22 @@ namespace BusinessLogic
         }
 
         // Employee Catalog metoder:
-        internal List <EmployeeE> GetEmployees()
+        public List <EmployeeE> GetEmployees()
         {
             return _empCat.GetEmployees();
         }
 
-        internal int NewEmployee(string cpr, string fName, string lName, string address, int postNo, 
+        public int NewEmployee(string cpr, string fName, string lName, string address, int postNo, 
                                  string eMail, string tlf, DateTime start, string position, decimal money)
         {
             return _empCat.NewEmployee(cpr, fName, lName, address, postNo, eMail, tlf, start, position, money);
         }
-        internal int UpdateEmployee(string address, int postNo, string eMail, string tlf, string position, decimal money)
+        public int UpdateEmployee(string address, int postNo, string eMail, string tlf, string position, decimal money)
         {
             return _empCat.UpdateEmployee(address, postNo, eMail, tlf, position, money);
         }
 
-        internal int CloseEmployee(int id)
+        public int CloseEmployee(int id)
         {
             return _empCat.CloseEmployee(id);
         }
@@ -45,50 +45,50 @@ namespace BusinessLogic
 
         // Employee metoder:
 
-        internal void InitializeEmployee(int id, string cpr, string fName, string lName, string address, int postNo,
+        public void InitializeEmployee(int id, string cpr, string fName, string lName, string address, int postNo,
                         string eMail, string tlf, DateTime start, string position, decimal money)
         {
            _employee.InitializeEmployee(id, cpr, fName, lName, address, postNo, eMail, tlf, start, position, money);
 
         }
-        internal List<FieldE> GetFields()
+        public List<FieldE> GetFields()
         {
             return _employee.GetFields();
         }
 
-        internal int NewField(string name)
+        public int NewField(string name)
         {
             return _employee.NewField(name);
         }
 
-        internal int AddField(string name)
+        public int AddField(string name)
         {
             return _employee.AddField(name);
         }
-        internal int DeleteField(string name)
+        public int DeleteField(string name)
         {
             return _employee.DeleteField(name);
         }
 
         // Legal Services metoder:
 
-        internal int NewLegalService(string name, int hours, int time, decimal price)
+        public int NewLegalService(string name, int hours, int time, decimal price)
         {
             return _serviceCat.NewLegalService(name, hours, time, price);
         }
 
-        internal List<LegalServiceE> GetLegalServices()
+        public List<LegalServiceE> GetLegalServices()
         {
             return _serviceCat.GetLegalServices();
         }
 
-        internal int UpdateLegalService(string name, int hours, int time, decimal price)
+        public int UpdateLegalService(string name, int hours, int time, decimal price)
         {
             return _serviceCat.UpdateLegalService(name, hours, time, price);
         }
 
 
-        internal int DeleteLegalService(int id)
+        public int DeleteLegalService(int id)
         {
             return _serviceCat.DeleteLegalService(id);
         }
