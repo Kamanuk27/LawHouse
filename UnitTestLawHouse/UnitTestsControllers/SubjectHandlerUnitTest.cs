98731e7e42555afd67414f460aed4be5edb4856d
Stanislava Yudytska <syudytska27@gmail.com>
2018-12-10 22:03:07 +0100

    Subjects tests klar, alle de unødvendige testklasser er slettet

---
+++ UnitTestLawHouse/UnitTestsControllers/SubjectHandlerUnitTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic;
+using LawHouseLibrary.Models;
+
+namespace UnitTestLawHouse.UnitTestsControllers
+{
+    [TestClass]
+    public class SubjectHandlerUnitTest
+    {
+        private SubjectHandler _subjectHandler = SubjectHandler.Instance;
+
+        [TestMethod]
+        public void TestSubjectHandlerReturnSameInstance()
+        {
+            SubjectHandler actual = new SubjectHandler();
+            object.ReferenceEquals(_subjectHandler, actual);
+
+        }
+
+        [TestMethod]
+        public void TestNewSubjectReturnOne()
+        {
+            int expected = 1;
+            int actual = _subjectHandler.NewSubject("Retssager", 30, 365, 0);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestGetSubjectsReturnTwoSubjects()
+        {
+            int expected = 2;
+            int actual = _subjectHandler.GetSubjects().Count;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestGetSubjectsReturnAListOfSubjects()
+        {
+            var subjects = _subjectHandler.GetSubjects();
+            Type expected = typeof(SubjectM);
+            Type actual = subjects[0].GetType();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestGetEmployeeSubjectsByIdReturnTwoSubjects()
+        {
+            int expected = 2;
+            int actual = _subjectHandler.GetEmployeeSubjectsById(1).Count;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestGetEmployeeSubjectsByIdReturnAListOfSubjects()
+        {
+            var subjects = _subjectHandler.GetEmployeeSubjectsById(1);
+            Type expected = typeof(SubjectM);
+            Type actual = subjects[0].GetType();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestUpdateSubjectReturnOne()
+        {
+            int expected = 1;
+            int actual = _subjectHandler.UpdateSubject(2, "Retssager/retshjælp", 20, 180, 0);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestDeleteSubjectReturnOne()
+        {
+            int expected = 1;
+            int actual = _subjectHandler.DeleteSubject(2);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ UnitTestLawHouse/UnitTestsCrudClasses/SubjectCrudUnitTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic;
+using LawHouseLibrary.Models;
+
+namespace UnitTestLawHouse.UnitTestsCrudClasses
+{
+    [TestClass]
+    public class SubjectCrudUnitTest
+    {
+        private SubjectCrud _subjectCrud = new SubjectCrud();
+
+        [TestMethod]
+        public void TestSubjectCrudVedInitializationDbSubjectIsNotNull()
+        {
+            var o = _subjectCrud._dbSubject;
+            Assert.IsNotNull(o);
+        }
+        [TestMethod]
+        public void TestNewSubjectReturnOne()
+        {
+            int expected = 1;
+            int actual = _subjectCrud.NewSubject("Retssager", 30, 365, 0);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestGetSubjectsReturnTwoSubjects()
+        {
+            int expected = 2;
+            int actual = _subjectCrud.GetSubjects().Count;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestGetSubjectsReturnAListOfSubjects()
+        {
+            var subjects = _subjectCrud.GetSubjects();
+            Type expected = typeof(SubjectM);
+            Type actual = subjects[0].GetType();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestGetEmployeeSubjectsByIdReturnTwoSubjects()
+        {
+            int expected = 2;
+            int actual = _subjectCrud.GetEmployeeSubjectsById(1).Count;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestGetEmployeeSubjectsByIdReturnAListOfSubjects()
+        {
+            var subjects = _subjectCrud.GetEmployeeSubjectsById(1);
+            Type expected = typeof(SubjectM);
+            Type actual = subjects[0].GetType();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestUpdateSubjectReturnOne()
+        {
+            int expected = 1;
+            int actual = _subjectCrud.UpdateSubject(2, "Retssager/retshjælp", 20, 180, 0);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestDeleteSubjectReturnOne()
+        {
+            int expected = 1;
+            int actual = _subjectCrud.DeleteSubject(2);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
